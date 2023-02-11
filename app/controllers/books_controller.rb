23b8e043c7471840cab036cdb04c7dfdb56c3392
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-02-11 13:01:57 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,12 @@ class BooksController < ApplicationController
   def edit
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def index
+     
   end
 
   def show
+     @user = User.find(params[:id])
+     @books = @user.books
 
   end
 
