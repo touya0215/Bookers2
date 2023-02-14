064abc23b160efbf01d180fb174724a43f9644e0
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-02-14 07:48:47 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
   def edit
 
   end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
 
   private
   def user_params
